439bda281552dd4fbce66cfa997ca54a82ab0d76
jay <jaylinuxmumbai@gmail.com>
2025-04-01 09:37:13 +0530

    day-5

--- day-4-statefile-locking/main.tf
+++ day-4-statefile-locking/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "dev" {
-    ami = "ami-071226ecf16aa7d96"
+    ami = "ami-076c6dbba59aa92e6"
     instance_type = "t2.micro"
     key_name = "aws_key"
     availability_zone = "ap-south-1a"
---
+++ day-5-statefile-overide/main.tf
@@ -0,0 +1,4 @@
+resource "aws_s3_bucket" "name" {
+    bucket = "viraj22viraj11-buckbukt-2"
+  
+}
