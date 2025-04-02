f3f898f67f0c11db18e3ecb248f0b557c0769abd
jay <jaylinuxmumbai@gmail.com>
2025-04-02 20:49:35 +0530

    aws-class-backend-2

---
+++ aws-class-backend-2/main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "name" {
+    ami = "ami-05c179eced2eb9b5b"
+    key_name = "aws_key"
+    instance_type = "t2.micro"
+    availability_zone = "ap-south-1a"
+    tags = {
+      Name = "Mumbai-Server"
+    }
+  
+}
