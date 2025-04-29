75514752bcd65cbaa1da5ae27cb779a951c8908a
jay <jaylinuxmumbai@gmail.com>
2025-04-29 16:03:34 +0530

    module-github-source

--- day-8-module-github-source/main.tf
+++ day-8-module-github-source/main.tf
@@ -1,6 +1,6 @@
 module "prod" {
     source = "github.com/virajgulhane2211/terraform-daywise/day-8-module-example-2/"
-    ami_id ="ami-0f1dcc636b69a6438"
+    ami_id ="ami-0e449927258d45bc4"
     instance_type = "t2.micro"
-    key_name = "aws_key"
+    key_name = "newkey"
 }
