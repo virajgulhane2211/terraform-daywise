8f17c6a51ccf1224f47cabf8996248fe35a9af39
jay <jaylinuxmumbai@gmail.com>
2025-06-09 17:37:59 +0530

    for-each means itteration

---
+++ day-11-for-each-part-3/main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+
+variable "ami" {
+  type    = string
+  default = "ami-05c179eced2eb9b5b"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "env" {
+  type    = list(string)
+  default = ["one","three"]
+}
+
+resource "aws_instance" "sandbox" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  for_each      = toset(var.env)
+#   count = length(var.env)  
+
+  tags = {
+    Name = each.value # for a set, each.value and each.key is the same
+  }
+}
