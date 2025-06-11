2520602bb5841d66197a03880bc3531dd0d08a2b
jay <jaylinuxmumbai@gmail.com>
2025-06-11 16:47:05 +0530

    dynamic block

---
+++ day-12-dynamic-block-part-4/variables.tf
@@ -0,0 +1,26 @@
+variable "ingress_rules" {
+  default = [
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["10.0.0.0/16"]
+    }
+  ]
+}
+  variable "egress_rules" {
+  default = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+}
